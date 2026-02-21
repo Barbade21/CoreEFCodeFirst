539be6a9f60577dc05de613c264def4623799560
Amol Barbade <barbade21@gmail.com>
2026-02-21 13:30:52 +0530

    Add project files.

---
+++ CoreEFCodeFirst/DataLayer/StudentContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoreEFCodeFirst.DataLayer
+{
+    public class StudentContext : DbContext
+    {
+
+        public StudentContext() { }
+
+        public StudentContext(DbContextOptions<StudentContext> options) : base(options) { }
+
+        public DbSet<Models.Student> Students { get; set; }
+
+    }
+}
